35c3f0e983ef6c91fa083fea2edd66c8384e72cc
Barak Harari <barakharari@gmail.com>
2020-07-08 16:52:58 +0000

    changes

--- api/routing.py
+++ api/routing.py
@@ -97,4 +97,4 @@ def getIngredientsByAisle(aisle):
     return jsonify({"ingredients": ingredients})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=False)
